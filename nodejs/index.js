45a2ae3e0197f1c8971939f6ddd9c42d45518af4
ninja-web-coder <67574086+ninja-web-coder@users.noreply.github.com>
2020-08-16 10:50:23 +0530

    Add files via upload

---
+++ nodejs/index.js
@@ -0,0 +1,3 @@
+const names = require('marvel-dc-name-generator');
+let name=names.generate();
+console.log(name);
---
+++ nodejs/node_modules/marvel-dc-name-generator/example/example.js
@@ -0,0 +1,5 @@
+const names = require('../lib/index');
+
+let name = names.generate();
+
+console.log(name);
